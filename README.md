cbcc5054ba28b15adb9951dbb5b198e81081fd0f
Eric MORAND <eric.morand@gmail.com>
2018-03-20 19:50:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+twig.js versus Twing benchmark
+==============================
+
+A comparison benchmark of the latest versions of [twig.js](https://github.com/twigjs/twig.js) and [Twing](https://github.com/ericmorand/twing).
+
+## The protocol
+
+This benchmark is based on the Smarty versus TwigPHP benchmark by [dominics](https://github.com/dominics/smarty-twig-benchmark):
+
+* Extending one base template and overriding its blocks
+* 3 blocks, with varying default content
+* A single for loop, outputting elements of an array within one block
+
+## Usage
+
+`npm install`
+
+`npm start`
+
+## Results
+
+On my machine, with twig.js 1.10.5 and Twing 0.10.2, node.js 9.8.0, 100000 iterations, compile time ignored, cache warmed:
+
+Benchmark | Time Taken
+--- | ---
+twig.js | ~37 seconds
+Twing | ~1 second
+
+As a matter of comparison, here are the results I get from Smarty versus TwigPHP benchmark on the same machine with TwigPHP 2.4.7, PHP 7.1.15, 100000 iterations, compile time ignored, cache warmed:
+
+Benchmark | Time Taken
+--- | ---
+TwigPHP | ~0.8 second
---
+++ index.js
@@ -0,0 +1,70 @@
+let Twig = require('twig');
+let Twing = require('twing');
+
+let setup = function(type) {
+    let template;
+
+    switch (type) {
+        case 'Twig': {
+            Twig.cache(true);
+
+            template = Twig.twig({
+                path: 'templates/index.html.twig',
+                async: false,
+                autoescape: true
+            });
+
+            break
+        }
+        case 'Twing': {
+            let loader = new Twing.TwingLoaderFilesystem('./templates');
+
+            let twing = new Twing.TwingEnvironment(loader, {
+                autoescape: 'html',
+                cache: 'cache'
+            });
+
+            template = twing.loadTemplate('index.html.twig');
+
+            break;
+        }
+    }
+
+    return template;
+};
+
+let benchmarkOnce = function(type, template, data) {
+    return template.render(data);
+};
+
+let benchmark = function(type, iterations = 100000) {
+    console.warn(`=== Benchmarking ${type} ===`);
+
+    let template = setup(type);
+    let data = {
+        'data': [
+            'some', 'bits', 'to', 'iterate', 'over'
+        ]
+    };
+
+    // Prime the cache
+    benchmarkOnce(type, template, data);
+
+    console.warn('>>> Cache warmed up');
+
+    let start = process.hrtime();
+
+    for (let i = 0; i < iterations; i++) {
+        benchmarkOnce(type, template, data);
+    }
+
+    let diff = process.hrtime(start);
+
+    let hrDiff = diff[0] * 1e9 + diff[1]; // nanoseconds
+
+    console.warn(`Time taken: ${hrDiff / 1000000000}s`);
+    console.warn(`Time taken per iteration: ${hrDiff / 1000000000 / iterations}s`);
+};
+
+benchmark('Twig');
+benchmark('Twing');
